5ca4f8c2abf248c9c6e7a2f559562ada2044f5e5
Cristian Di Grandi <write2cmdg@gmail.com>
2023-01-30 16:27:32 -0500

    added middleware to allow crud connections

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ app.use(session({
     cookie: { maxAge: seshLength }, 
     resave: false
 }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+})
 
 
 
